25b0ceb04d005826f6e1880d30a0b1d85cdd5dfc
Camilo Pacheco <LutzMage@users.noreply.github.com>
2017-03-22 20:00:26 -0500

    Ejercico 2026 INCOMPLETO

---
+++ 2026.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define REP(i,a,n) for(int i = a; i<n; i++)
+#define MAXP 101
+#define MAXW 1001
+#define pb push_back
+
+using namespace std;
+typedef vector<int> vi;
+
+int dp[MAXP][MAXW];
+vi pes(MAXP);
+vi es(MAXP);
+int peso,p,w;
+
+int solve(int i,int w)
+{
+    if(i==p || w==0)
+        return 0;
+    if(dp[i][w]!=-1)
+        return dp[i][w];
+    int res = 0;
+    if(pes[i]>w)
+        res = solve(i+1,w);
+    else
+        res = max(solve(i+1,w),es[i]+solve(i+1,w-pes[i]));
+    return dp[i][w] = res;
+}
+
+void llenar(int p){
+    REP(i,0,p){
+        REP(j,0,w){
+            dp[i][j]=-1;
+        }
+    }
+}
+
+int main()
+{
+    int casos,ramas,peso,e;
+    scanf("%d",&casos);
+
+    REP(i,0,casos)
+    {
+        scanf("%d %d",&p,&peso);
+        llenar(p);
+        REP(j,0,p)
+        {
+            scanf("%d %d",&e,&w);
+            pes[j]=w;
+            es[j]=e;
+        }
+
+        printf("Galho %d:\nNumero total de enfeites: %d\n",(i+1),solve(0,peso));
+    }
+
+    return 0;
+}
