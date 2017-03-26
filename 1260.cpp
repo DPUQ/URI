f89209432f1f0f4bddf94c1de672cc1612b087ca
LutzMage <lutz.mage@gmail.com>
2017-03-26 14:29:30 -0500

    Ejercicio 1261

---
+++ 1260.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define e end()
+#define b begin()
+#define f find
+#define pb push_back
+#define cl clear()
+#define s size()
+#define en "\n"
+#define pdec(n) fixed << setprecision(n)
+
+using namespace std;
+typedef string st;
+typedef vector<st> vs;
+typedef long long ll;
+typedef map<st,ll> msl;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    st cur;
+    msl forest;
+    msl::iterator it;
+    int casos,cont=0;
+    long total;
+
+    cin >> casos; cin.ignore();
+
+    while(casos--)
+    {
+
+        forest.cl;
+        total = 0;
+
+        if (cont != 0)
+        {
+            cout << en;
+        }
+        else
+        {
+            getline(cin,cur);
+        }
+
+        while(getline(cin,cur) && cur[0]!='\0')
+        {
+            forest[cur]++;
+            total++;
+        }
+
+       for (it = forest.b; it != forest.e; it++)
+            cout << it->first << " " << pdec(4) << (it->second*100.0)/total << endl;
+
+        cont++;
+    }
+
+
+    return 0;
+}
